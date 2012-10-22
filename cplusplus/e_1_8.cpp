36fe1078f5ea65b8004273e24649cdf5d8f1ccc2
xmuliushuo <xmuliushuo@gmail.com>
2012-10-22 20:50:58 +0800

    c++ primer

---
+++ cplusplus/e_1_8.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+void e_1_8()
+{
+    std::cout << "/*";
+    std::cout << "*/";
+    std::cout << /* "*/" */";
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2012-10-22
+ *      Author: liushuo
+ */
+
+void e_1_8();
+
+int main()
+{
+	e_1_8();
+	return 0;
+}
+
+
+
