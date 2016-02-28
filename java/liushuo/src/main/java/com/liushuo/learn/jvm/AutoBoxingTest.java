76f5abb9e5c0a1f699e76463261f68a43809a2ef
xmuliushuo <liushuo@liushuodeMacBook-Pro.local>
2016-02-28 16:41:35 +0800

    modify

---
+++ java/liushuo/src/main/java/com/liushuo/learn/jvm/AutoBoxingTest.java
@@ -0,0 +1,22 @@
+package com.liushuo.learn.jvm;
+
+/**
+ * Created by liushuo on 16/2/28.
+ */
+public class AutoBoxingTest {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d); // true
+        System.out.println(e == f); // false jvm缓存-128 ~ 127
+        System.out.println(c == (a + b)); // true
+        System.out.println(c.equals(a + b)); // true
+        System.out.println(g == (a + b)); // true
+        System.out.println(g.equals(a + b)); // false
+    }
+}
